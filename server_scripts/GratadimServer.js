a65c6bc5d03d5fa537fc7a2fd9cb52889d2e0910
SiwelTheLongBoi <siwel99@yahoo.com>
2026-01-05 00:44:46 +0000

    setup

---
+++ server_scripts/GratadimServer.js
@@ -0,0 +1,10 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded server scripts)')
+
+ServerEvents.recipes(event => {
+
+console.log('Hello! The recipe event has fired!')
+})
